0b1d4bc58c953450077738e3d66c78b124a13540
elves brito <elvesbd@yahoo.com>
2021-03-20 20:31:03 -0300

    finished challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import React from 'react';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    title: string;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  }>
+  selectedGenreId: number;
+  handleClickButton: Function;
+}
+
+
+export function SideBar({ genres, handleClickButton, selectedGenreId } : SideBarProps) {
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          id={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+  );
 }
